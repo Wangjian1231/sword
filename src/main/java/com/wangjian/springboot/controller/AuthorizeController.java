0bd03d0622ad60c9a1d52e6f7ed95d7144ae2dcc
wangjian <9839984272qq.com>
2019-05-04 22:51:45 +0800

    complete login index

--- src/main/java/com/wangjian/springboot/controller/AuthorizeController.java
+++ src/main/java/com/wangjian/springboot/controller/AuthorizeController.java
@@ -3,11 +3,14 @@ package com.wangjian.springboot.controller;
 import com.wangjian.springboot.dto.AccessTokenDTO;
 import com.wangjian.springboot.dto.GithubUser;
 import com.wangjian.springboot.provider.GithubProvider;
+import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * ClassName:AuthorizeController
@@ -32,8 +35,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state
-                           ){
+                           @RequestParam(name = "state") String state,
+                                   HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -42,7 +45,14 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user !=null){
+            request.getSession().setAttribute("user",user);
+            //登录成功   写cookie 和session
+            return "redirect:/";
+        }else{
+            //登录失败 重新登录
+            return "redirect:/";
+        }
+
     }
 }
